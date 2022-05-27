d91decc6b523f030b747dddc286a4625794ed09c
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-05-27 10:29:49 -0600

    feat: :sparkles: Create service of weather - Add method to get current weather

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherDataToSend, WeatherForecastResponse } from '../models/weather';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  baseUrl = environment.api.weather;
+  constructor(private http: HttpClient) {}
+
+  getForecastWeather(
+    data: WeatherDataToSend
+  ): Observable<WeatherForecastResponse> {
+    return this.http.get<WeatherForecastResponse>(
+      `${this.baseUrl.url_forecast}?key=${this.baseUrl.key}&q=${data.query}&days=${data.days}`
+    );
+  }
+}
