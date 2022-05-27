6115d533c815ef39d399ce4a0a990be82f1bebff
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-05-27 10:26:59 -0600

    feat: :sparkles: Add model of geolocation

---
+++ src/app/models/geolocation.ts
@@ -0,0 +1,13 @@
+export interface GeolocationResponse {
+  city: string;
+  country_code: string;
+  country_name: string;
+  ip: string;
+  latitude: number;
+  longitude: number;
+  metro_code: number;
+  region_code: string;
+  region_name: string;
+  time_zone: string;
+  zip_code: string;
+}
