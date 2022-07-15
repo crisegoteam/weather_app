7a8d71f264d1d8f713d72394c7bedb2786c9fab0
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-07-15 15:02:44 -0600

    fix: :sparkles: Change gelocation model response

--- src/app/models/geolocation.ts
+++ src/app/models/geolocation.ts
@@ -1,13 +1,28 @@
 export interface GeolocationResponse {
-  city: string;
-  country_code: string;
-  country_name: string;
   ip: string;
+  version: string;
+  city: string;
+  region: string;
+  region_code: string;
+  country: string;
+  country_name: string;
+  country_code: string;
+  country_code_iso3: string;
+  country_capital: string;
+  country_tld: string;
+  continent_code: string;
+  in_eu: boolean;
+  postal: any;
   latitude: number;
   longitude: number;
-  metro_code: number;
-  region_code: string;
-  region_name: string;
-  time_zone: string;
-  zip_code: string;
+  timezone: string;
+  utc_offset: string;
+  country_calling_code: string;
+  currency: string;
+  currency_name: string;
+  languages: string;
+  country_area: number;
+  country_population: number;
+  asn: string;
+  org: string;
 }
